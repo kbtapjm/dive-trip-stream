601874bfdb27a55bd036181c097985c264062529
jmpark <kbtapjm@gmail.com>
2024-12-04 15:26:30 +0900

    #1 kafka configuration

--- src/main/java/io/divetrip/kafka/KafkaConsumer.java
+++ src/main/java/io/divetrip/kafka/KafkaConsumer.java
@@ -1,5 +1,8 @@
 package io.divetrip.kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.divetrip.dto.request.ProductCreateDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,8 +13,20 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class KafkaConsumer {
 
+    private final ObjectMapper mapper;
+
     @KafkaListener(topics = "dev-topic")
     public void devTopicListener(String kafkaMessage) {
         log.debug("Kafka Message : -> " + kafkaMessage);
+
+        try {
+            ProductCreateDto productCreateDto = mapper.readValue(kafkaMessage, ProductCreateDto.class);
+            log.debug("===> getDiverId: {}", productCreateDto.getDiverId());
+            log.debug("===> getTripLodgingId: {}", productCreateDto.getTripLodgingId());
+        } catch (JsonProcessingException e) {
+            log.error("JsonProcessingException: {}", e.getMessage(), e);
+        }
+
+
     }
 }
